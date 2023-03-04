d3b63d8d25ef32832191ae35be366d8182e0dbaa
Lpik97 <Lpik97@users.noreply.github.com>
2023-03-04 10:43:52 +0100

    The gridContainer now has as style both columns and rows usign the repeat shorthand with a the input size. That input is the same that needs to be inserted in order to run the entire function of addDivs.

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-const h1 = document.createElement('h1');
-const gridContainer = document.createElement('div');
-h1.className = 'h1';
-h1.innerText = 'Etch-a-Sketch';
-gridContainer.className = 'gridContainer';
-document.body.appendChild(h1);
-document.body.appendChild(gridContainer);
-for (let i = 0; i < 256; i++) {
-    let gridDiv = document.createElement('div');
-    gridDiv.className = "gridDiv";
-    gridContainer.appendChild(gridDiv);
+function addDivs (size) {
+    const h1 = document.createElement('h1');
+    const gridContainer = document.createElement('div');
+    h1.className = 'h1';
+    h1.innerText = 'Etch-a-Sketch';
+    gridContainer.className = 'gridContainer';
+    gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    document.body.appendChild(h1);
+    document.body.appendChild(gridContainer);
+
+    for (let i = 0; i < 256; i++) {
+        let gridDiv = document.createElement('div');
+        gridDiv.className = "gridDiv";
+        gridContainer.appendChild(gridDiv);
+    }
 }
+addDivs(16);
+
+
