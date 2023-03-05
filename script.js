560791cca7d595e8af6c59cdcf4dd1bb11befe2b
Lpik97 <Lpik97@users.noreply.github.com>
2023-03-05 10:27:26 +0100

    A button was added with an event listener waiting for a click to exectue the createGrid function. The createGrid function will ask for a number between 2-100 in a prompt and then check if that number is higher than 2 an not higher than 100. If that is the case, it will ask you again.

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const askingButton = document.createElement('button');
+askingButton.innerText = 'Select a number';
+askingButton.addEventListener('click', createGrid);
+document.body.appendChild(askingButton);
+
 function addDivs (size) {
     const h1 = document.createElement('h1');
     const gridContainer = document.createElement('div');
@@ -5,18 +10,30 @@ function addDivs (size) {
     h1.className = 'h1';
     h1.innerText = 'Etch-a-Sketch';
     gridContainer.className = 'gridContainer';
-    gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-    gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
     document.body.appendChild(h1);
     document.body.appendChild(gridContainer);
+    gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
     for (let i = 0; i < totalPixels; i++) {
         let gridDiv = document.createElement('div');
-        gridDiv.className = "gridDiv";
+        gridDiv.addEventListener('mouseover', function () {
+            gridDiv.style.backgroundColor = 'black';  
+        });
         gridContainer.appendChild(gridDiv);
     }
 }
-addDivs(16);
+
+function createGrid () {
+   let size = Number(prompt('Select a number between 2 and 100', '2-100'));
+    if (size >= 2 && size <= 100) {
+        addDivs(size);
+    } else {
+        alert('You need to select a number between 2 and 100, try again');
+        createGrid();
+    }
+}
+
 
 
 
