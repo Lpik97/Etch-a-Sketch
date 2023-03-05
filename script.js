b3ad9a5efc464c7099c8de1991f62089ce5702a7
Lpik97 <Lpik97@users.noreply.github.com>
2023-03-05 13:49:08 +0100

    The buttons were added inside a button container to be styled. A new button was added with the color black to change the color of the divs background to black.

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
-const askingButton = document.createElement('button');
-askingButton.className = 'askingButton';
-askingButton.innerText = 'Select a number';
-askingButton.addEventListener('click', createGrid);
-document.body.appendChild(askingButton);
-
 const h1 = document.createElement('h1');
 const gridContainer = document.createElement('div');
+const buttonContainer = document.createElement('div');
 h1.className = 'h1';
 h1.innerText = 'Etch-a-Sketch';
 gridContainer.className = 'gridContainer';
-document.body.insertBefore(h1, askingButton);
+buttonContainer.className = 'buttonContainer';
+document.body.appendChild(h1);
 document.body.appendChild(gridContainer);
+document.body.appendChild(buttonContainer);
+
+const askingButton = document.createElement('button');
+askingButton.className = 'askingButton';
+askingButton.innerText = 'GRID SIZE';
+askingButton.addEventListener('click', createGrid);
+buttonContainer.appendChild(askingButton);
+
+const colorButton = document.createElement ('button');
+colorButton.className = 'colorButton';
+colorButton.innerText = 'RANDOM COLOR';
+colorButton.addEventListener('click', generateColor);
+buttonContainer.appendChild(colorButton);
+
+const blackButton = document.createElement ('button');
+blackButton.className = 'blackButton';
+blackButton.innerText = 'BLACK';
+blackButton.addEventListener('click', generateColor);
+buttonContainer.appendChild(blackButton);
 
 function addDivs (size) {
     const totalPixels = (size * size); 
@@ -21,6 +36,7 @@ function addDivs (size) {
     for (let i = 0; i < totalPixels; i++) {
         let gridDiv = document.createElement('div');
         gridDiv.addEventListener('mouseover', function () {
+            if ()
             gridDiv.style.backgroundColor = generateColor();  
         });
         gridContainer.appendChild(gridDiv);
@@ -28,7 +44,7 @@ function addDivs (size) {
 }
 
 function createGrid () {
-   let size = Number(prompt('Select a number between 2 and 100', '2-100'));
+   let size = Number(prompt('Select a number between 2 and 100', 'e.g: 16'));
     if (size >= 2 && size <= 100) {
         addDivs(size);
     } else {
@@ -41,6 +57,8 @@ function generateColor () {
     let red = Math.floor(Math.random() * 256);
     let green = Math.floor(Math.random() * 256);
     let blue = Math.floor(Math.random() * 256);
-    let color = `rgb(${red}, ${green}, ${blue})`;
-    return color; 
+    let alpha = Math.random();
+    let color = `rgb(${red}, ${green}, ${blue}, ${alpha})`;
+    return color;
 }
+
