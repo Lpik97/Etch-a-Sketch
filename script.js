11ed482cd537429f94ac855fc24abf1349e6ecb7
Lpik97 <Lpik97@users.noreply.github.com>
2023-03-05 11:52:06 +0100

    The creation of both h1 and gridContainer now are out of the addDivs function as the grid has to be always on the webpage. The addDivs function now will add a innerHTML of an empty string to delete all the previous black divs inside the grid.

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 const askingButton = document.createElement('button');
+askingButton.className = 'askingButton';
 askingButton.innerText = 'Select a number';
 askingButton.addEventListener('click', createGrid);
 document.body.appendChild(askingButton);
 
+const h1 = document.createElement('h1');
+const gridContainer = document.createElement('div');
+h1.className = 'h1';
+h1.innerText = 'Etch-a-Sketch';
+gridContainer.className = 'gridContainer';
+document.body.insertBefore(h1, askingButton);
+document.body.appendChild(gridContainer);
+
 function addDivs (size) {
-    const h1 = document.createElement('h1');
-    const gridContainer = document.createElement('div');
-    const totalPixels = (size * size);
-    h1.className = 'h1';
-    h1.innerText = 'Etch-a-Sketch';
-    gridContainer.className = 'gridContainer';
-    document.body.appendChild(h1);
-    document.body.appendChild(gridContainer);
+    const totalPixels = (size * size); 
+    gridContainer.innerHTML = '';
     gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
@@ -33,8 +36,3 @@ function createGrid () {
         createGrid();
     }
 }
-
-
-
-
-
