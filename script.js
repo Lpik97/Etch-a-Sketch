ce817c64128279616646cc4b148c4638122c3f01
Lpik97 <Lpik97@users.noreply.github.com>
2023-03-06 08:54:46 +0100

    Opacity was added to the blackMode, making the user capable of change the divs color only by hovering the div again. Comments were added to better explain the code functionallity.

--- script.js
+++ script.js
@@ -33,23 +33,29 @@ eraseButton.innerText = 'ERASE';
 eraseButton.addEventListener('click', enableEraseMode);
 buttonContainer.appendChild(eraseButton);
 
-let colorMode = false;
+let colorMode = false; //All three modes are turn-off by default, only by clicking one of the buttons it will call the functions to turn them on.
 let blackMode = false;
 let eraseMode = false;
 
 function addDivs (size) {
-    const totalPixels = (size * size); 
-    gridContainer.innerHTML = '';
+    const totalPixels = (size * size); //The total amount of pixels is the result of multiply the number by itself.
+    gridContainer.innerHTML = ''; //Everytime addDivs is called, the gridContainer will delete all the divs inside it.
     gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
     for (let i = 0; i < totalPixels; i++) {
         let gridDiv = document.createElement('div');
+        gridDiv.dataset.opacity = 0; //A data attribute is added to assign the initial opacity of the divs.
         gridDiv.addEventListener('mouseover', function () {
             if (colorMode) {
                 gridDiv.style.backgroundColor = generateColor();
             } else if (blackMode) {
-                gridDiv.style.backgroundColor = 'black';
+                let currentOpacity = parseFloat(gridDiv.dataset.opacity); //The parseFloat method will turn the string-number of the data attribute into a real number.
+                if (currentOpacity < 1) { //If the currentOpacity is less than 1, a 0.1 will be sum and added to the total.
+                    currentOpacity += 0.1;
+                    gridDiv.dataset.opacity = currentOpacity; //The actual opacity is assgined directly to the data attribute.
+                    gridDiv.style.backgroundColor = `rgb(0, 0, 0, ${currentOpacity})`; //And the variable is going inside the rgb color format.
+                }
             } else if (eraseMode) {
                 gridDiv.style.backgroundColor = 'white';
             }
@@ -77,7 +83,7 @@ function generateColor () {
     return color;
 }
 
-function enableColorMode () {
+function enableColorMode () { //The reason to declare the other modes is to be sure that they are turn off, specially when you switch modes.
     colorMode = true;
     blackMode = false;
     eraseMode = false;
